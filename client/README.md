481362c5f1f0f0445706d52d6b8d482b56273074
Bryan Baird <bryan.eric.baird@gmail.com>
2024-08-17 17:42:41 -0400

    Primary commit to bring over Svelte client app code
    Copying from other projects/explorations, where this logic was originally being developed in the context of a SvelteKit app. There may still be some unexpected growing pains or extra leftovers as a result, but initial testing shows Flask-serving working as expected. Also updates documentation accordingly.

--- client/README.md
+++ client/README.md
@@ -1,107 +1,17 @@
-# This repo is no longer maintained. Consider using `npm init vite` and selecting the `svelte` option or — if you want a full-fledged app framework — use [SvelteKit](https://kit.svelte.dev), the official application framework for Svelte.
+# Client app (Svelte frontend)
 
----
+## Contents
+ - The `public` directory contains all assets that are accessible to the end user/client. This includes static html, universal style files, supporting images, etc.
+   - The `public/build` sub-directory includes the compiled output JavaScript and other resources produced by Svelte.
+   - `public/index.html` is the placeholder for the main index page. Changes made here _will_ show up in the ultimately served application, but it is much better to make changes within the Svelte framework under `src`.
+ - The `src` directory includes the Svelte components that form the bulk of the client app design and functionality. **Look in this directory for the "meat" of the app to make changes.**
+   - `src/main.js` is just the basic wrapper that calls the client app. It is best to not touch this unless much more complexity and compartamentalization logic is needed.
+   - `src/App.svelte` is the primary application code assembled as a Svelte module -- HTML, JavaScript interactive components, and CSS styling.
 
-# svelte app
+## Making changes
+Svelte is a complied language, with the output being "vanilla" JavaScript, HTML, and CSS. After making a modification to anything in the `src` directory, you will need to re-build by running the following commmand from the command line _within this directory_.
+```npm run build```
+(This assumes npm and Svelte are both installed. Details on that to come.)
 
-This is a project template for [Svelte](https://svelte.dev) apps. It lives at https://github.com/sveltejs/template.
-
-To create a new project based on this template using [degit](https://github.com/Rich-Harris/degit):
-
-```bash
-npx degit sveltejs/template svelte-app
-cd svelte-app
-```
-
-*Note that you will need to have [Node.js](https://nodejs.org) installed.*
-
-
-## Get started
-
-Install the dependencies...
-
-```bash
-cd svelte-app
-npm install
-```
-
-...then start [Rollup](https://rollupjs.org):
-
-```bash
-npm run dev
-```
-
-Navigate to [localhost:8080](http://localhost:8080). You should see your app running. Edit a component file in `src`, save it, and reload the page to see your changes.
-
-By default, the server will only respond to requests from localhost. To allow connections from other computers, edit the `sirv` commands in package.json to include the option `--host 0.0.0.0`.
-
-If you're using [Visual Studio Code](https://code.visualstudio.com/) we recommend installing the official extension [Svelte for VS Code](https://marketplace.visualstudio.com/items?itemName=svelte.svelte-vscode). If you are using other editors you may need to install a plugin in order to get syntax highlighting and intellisense.
-
-## Building and running in production mode
-
-To create an optimised version of the app:
-
-```bash
-npm run build
-```
-
-You can run the newly built app with `npm run start`. This uses [sirv](https://github.com/lukeed/sirv), which is included in your package.json's `dependencies` so that the app will work when you deploy to platforms like [Heroku](https://heroku.com).
-
-
-## Single-page app mode
-
-By default, sirv will only respond to requests that match files in `public`. This is to maximise compatibility with static fileservers, allowing you to deploy your app anywhere.
-
-If you're building a single-page app (SPA) with multiple routes, sirv needs to be able to respond to requests for *any* path. You can make it so by editing the `"start"` command in package.json:
-
-```js
-"start": "sirv public --single"
-```
-
-## Using TypeScript
-
-This template comes with a script to set up a TypeScript development environment, you can run it immediately after cloning the template with:
-
-```bash
-node scripts/setupTypeScript.js
-```
-
-Or remove the script via:
-
-```bash
-rm scripts/setupTypeScript.js
-```
-
-If you want to use `baseUrl` or `path` aliases within your `tsconfig`, you need to set up `@rollup/plugin-alias` to tell Rollup to resolve the aliases. For more info, see [this StackOverflow question](https://stackoverflow.com/questions/63427935/setup-tsconfig-path-in-svelte).
-
-## Deploying to the web
-
-### With [Vercel](https://vercel.com)
-
-Install `vercel` if you haven't already:
-
-```bash
-npm install -g vercel
-```
-
-Then, from within your project folder:
-
-```bash
-cd public
-vercel deploy --name my-project
-```
-
-### With [surge](https://surge.sh/)
-
-Install `surge` if you haven't already:
-
-```bash
-npm install -g surge
-```
-
-Then, from within your project folder:
-
-```bash
-npm run build
-surge public my-project.surge.sh
-```
+## Key dependencies
+ - The "xlsx" library here is the main pillar for initially parsing in the input Excel file to a JavaScript Array of Arrays that can then be manipulated using vanilla JavaScript logic. (Or, if needed in the future, some other JavaScript library/structure that is more suited to tabular data structures, a la Python's `pandas` or R's dataframes.)
--- client/src/App.svelte
+++ client/src/App.svelte
@@ -1,30 +1,223 @@
 <script>
-	export let name;
-</script>
-
-<main>
-	<h1>Hello {name}!</h1>
-	<p>Visit the <a href="https://svelte.dev/tutorial">Svelte tutorial</a> to learn how to build Svelte apps.</p>
-</main>
-
-<style>
-	main {
-		text-align: center;
-		padding: 1em;
-		max-width: 240px;
-		margin: 0 auto;
+	import { read, utils } from "xlsx";
+	let files, fileInput;
+	let workbook;
+	let trimmed_array = [];
+	let trimmed_header_rows = [];
+	let trimmed_trailing_rows = [];
+	$: trimmed_header_rows_count = trimmed_header_rows.length;
+	$: trimmed_trailing_rows_count = trimmed_trailing_rows.length;
+  
+	const COMPLETE_COLUMNS_RATIO = 0.9;
+	const DATE_COLUMN_MAPPINGS = {
+	  Year: "year",
+	  Month: "month",
+	  annee: "year",
+	  mois: "month",
+	  period: "date",
+	  periode: "date",
+	  date: "date",
+	  quarter: "quarter",
+	};
+  
+	const CUSTOM_INDEX_COLUMN_MAPPINGS = {
+	  "Layer 1": "index_level_1",
+	  "Layer 2": "index_level_2",
+	  "Layer 3": "index_level_3",
+	};
+  
+	const ALL_INDEX_COLUMN_MAPPINGS = {
+	  ...DATE_COLUMN_MAPPINGS,
+	  ...CUSTOM_INDEX_COLUMN_MAPPINGS,
+	};
+  
+	let detected_index_columns = [];
+  
+	$: if (files) {
+	  console.log(`${files[0].name}`);
 	}
-
-	h1 {
-		color: #ff3e00;
-		text-transform: uppercase;
-		font-size: 4em;
-		font-weight: 100;
+  
+	$: active_sheet = workbook ? workbook.Sheets[workbook.SheetNames[0]] : null;
+  
+	$: parsed_original_HTML = active_sheet
+	  ? utils.sheet_to_html(active_sheet)
+	  : null;
+  
+	$: trimmed_data_as_HTML_table = trimmed_array.length
+	  ? utils.sheet_to_html(utils.aoa_to_sheet(trimmed_array))
+	  : null;
+  
+	function parseExcelFile(my_file) {
+	  console.log("inside parse function with parameter my_file below:");
+	  console.log(my_file);
+	  var reader = new FileReader();
+	  reader.onload = (e) => {
+		let data = e.target.result;
+		workbook = read(data);
+		console.log("workbook object read from data:");
+		console.log(workbook);
+	  };
+	  reader.readAsArrayBuffer(my_file);
 	}
-
-	@media (min-width: 640px) {
-		main {
-			max-width: none;
+  
+	function trimHeaderRows(dataframe) {
+	  console.log("Here's what we're working with:");
+	  console.log(dataframe);
+	  let df = utils.sheet_to_json(dataframe, { header: 1 });
+	  let full_cols_count = count_columns(df);
+	  let COMPLETE_COLUMNS_THRESHOLD = full_cols_count * COMPLETE_COLUMNS_RATIO;
+	  let first_true_row_index = 0;
+	  for (let i = 0; i <= df.length; i++) {
+		// TODO: Filter out datum entries that don't "count" as "real" content
+		if (df[i].length >= COMPLETE_COLUMNS_THRESHOLD) {
+		  first_true_row_index = i;
+		  break;
 		}
+	  }
+	  return [
+		/*remaining:*/ df.slice(first_true_row_index, df.length),
+		/*removed:*/ df.slice(0, first_true_row_index),
+	  ];
 	}
-</style>
+  
+	function getIndexColumns(dataframe) {
+	  let header_row = dataframe[0];
+	  let matched_raw_values = [];
+	  let matched_col_indeces = [];
+	  let matched_standardized_values = [];
+	  header_row.forEach((element, index) => {
+		if (Object.keys(ALL_INDEX_COLUMN_MAPPINGS).includes(element)) {
+		  matched_raw_values.push(element);
+		  matched_col_indeces.push(index);
+		  matched_standardized_values.push(ALL_INDEX_COLUMN_MAPPINGS[element]);
+		}
+	  });
+  
+	  return [
+		matched_raw_values,
+		matched_col_indeces,
+		matched_standardized_values,
+	  ];
+	}
+  
+	function trimTrailingRows(dataframe) {
+	  console.log("Here's what we're working with after headers:");
+	  console.log(dataframe);
+	  let last_true_row_index = dataframe.length - 1;
+	  //debugger;
+	  for (let i = dataframe.length - 1; i >= 0; i--) {
+		// A valid ending row has nonnull values for all index columns
+		// We can be even more expansive and say that all index columns need
+		// to have JS "truthy" values to avoid issues of empty strings, etc.
+		let detected_index_values = detected_index_columns[1].map(
+		  (x) => dataframe[i][x]
+		);
+		if (detected_index_values.every(Boolean)) {
+		  last_true_row_index = i;
+		  break;
+		}
+	  }
+	  let output = [
+		/*remaining:*/ dataframe.slice(0, last_true_row_index + 1),
+		/*removed:*/ dataframe.slice(last_true_row_index + 1, dataframe.length),
+	  ];
+	  return output;
+	}
+  
+	function count_columns(dataframe) {
+	  return dataframe.reduce(
+		(maxLen, el, i, arr) => (el.length > maxLen ? el.length : maxLen),
+		0
+	  );
+	}
+  
+	function get_fake_columns(dataframe) {
+	  // We define "fake" columns as a column that is either:
+	  // 1) Blank for all "real" rows (as defined above)
+	  // 2) Only has "fake" values, that are functionaly equivalent to blank
+	  var fake_values = ['-', '/', 'NA', 'N/A', 'None', '.']
+	  // Important to note for point (2) does *not* apply to all zeroes --
+	  // That may be "real" 0s, or it could be "fake"; it can't be immediately
+	  // determined without context.
+	  
+  
+	}
+  </script>
+  
+  <svelte:head>
+	<title>Excel Parser</title>
+	<meta name="excel-parser" content="Transform Excel Files" />
+  </svelte:head>
+  
+  <div class="text-column">
+	<h1>Excel Parser</h1>
+  
+	<p>
+	  Upload an Excel file to this tab to see it quickly transformed into an HTML
+	  object.
+	</p>
+  
+	<p>
+	  Then, step-by-step cleaning functions will help transform it to a clean,
+	  tall table, accounting for some common cases along the way.
+	</p>
+  
+	<label for="file_picker">Select an data file to process: </label>
+	<input
+	  bind:files
+	  bind:this={fileInput}
+	  type="file"
+	  id="file_picker"
+	  name="file_picker"
+	  accept=".xls,.xlsx, application/vnd.openxmlformats-officedocument.spreadsheetml.sheet,application/vnd.ms-excel"
+	/>
+  
+	{#if files}
+	  <h3>Selected file:</h3>
+	  {files[0].name}
+	  <button
+		on:click={() => {
+		  console.log(`${files[0].name} will be processed`);
+		  parseExcelFile(files[0]);
+		  console.log(workbook);
+		}}
+	  >
+		Confirm and process
+	  </button>
+	{/if}
+	{#if parsed_original_HTML}
+	  <h3>Input data</h3>
+	  <div id="step-1-data" class="big-html-table">
+		{@html parsed_original_HTML}
+	  </div>
+	  <button
+		on:click={() => {
+		  [trimmed_array, trimmed_header_rows] = trimHeaderRows(active_sheet);
+		  //trimmed_header_row_count = trimmed_header_rows.length
+		  detected_index_columns = getIndexColumns(trimmed_array);
+		  [trimmed_array, trimmed_trailing_rows] =
+			trimTrailingRows(trimmed_array);
+		  debugger;
+		}}
+	  >
+		Trim excess header and trailing rows from this table
+	  </button>
+	{/if}
+	{#if trimmed_array.length}
+	  {trimmed_header_rows_count} excess row(s) were removed the top of the sheet,
+	  and {trimmed_trailing_rows_count} row(s) were removed from the bottom. The trimmed
+	  table now looks like this:
+	  <div id="step-2-data" class="big-html-table">
+		{@html trimmed_data_as_HTML_table}
+	  </div>
+	{/if}
+  </div>
+  
+  <style>
+	div.big-html-table {
+	  height: 230px;
+	  width: 900px;
+	  overflow: scroll;
+	}
+  </style>
+  
