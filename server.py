2987c1c4f3577aa03ab6c91f581194beefc76775
Bryan Baird <bryan.eric.baird@gmail.com>
2024-08-18 09:05:46 -0400

    Documentation and formatting cleanup

--- server.py
+++ server.py
@@ -3,18 +3,20 @@ from flask import Flask, send_from_directory
 app = Flask(__name__)
 
 
-@app.route('/')
+@app.route("/")
 def home():
     """Serve the main app page. The main page will be 'static' in the sense of being purely client-side Svelte. So no further paramters or returns are needed."""
-    return send_from_directory('client/public', 'index.html')
+    return send_from_directory("client/public", "index.html")
+
 
 # Path for all the static files (compiled JS/CSS, etc.)
 @app.route("/<path:path>")
 def static_files(path):
-    return send_from_directory('client/public', path)
+    return send_from_directory("client/public", path)
 
-@app.route('/upload')
-def submit_data(credential: str, endpoint: str, payload:str) -> str:
+
+@app.route("/upload")
+def submit_data(credential: str, endpoint: str, payload: str) -> str:
     """Take some object or payload from the frontend and send it off somewhere as an upload. The logic of where and how is TBD.
 
     Args:
@@ -25,7 +27,8 @@ def submit_data(credential: str, endpoint: str, payload:str) -> str:
     Returns:
         str: _description_
     """
-    return('Good job')
+    return "Good job"
+
 
 if __name__ == "__main__":
     app.run(debug=True)
